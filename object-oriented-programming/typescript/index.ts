088bf650eba28e5bef6125800a6362b4dd48570c
Tomas Coto <tomascoto856@gmail.com>
2025-03-31 15:17:19 -0600

    feat(typescript): add index file to the folder

--- object-oriented-programming/typescript/index.ts
+++ object-oriented-programming/typescript/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello Wolrd!!')
+import { Prompt } from './../../node_modules/@types/prompt-sync/index.d';
+interface Account{
+    id: string | number,
+    name: string,
+    balance: number
+    
+}
+
+const createAccount = (id: number, name: string, balance: number): Account =>{
+    return{
+        id,
+        name,
+        balance
+    }
+}
+
+const acc1 = createAccount(1,'', 0)
+
+import * as PromptSync from 'prompt-sync';
+const prompt = PromptSync();
+
+const result = prompt('message: ')
+
+console.log(result)
